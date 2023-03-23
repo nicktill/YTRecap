acc493b14fa13b2e6abb469a355d9fc1162a576a
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-03-23 15:12:33 -0400

    updates

--- yt-recap/transcript.py
+++ yt-recap/transcript.py
@@ -8,11 +8,14 @@ import datetime
 import isodate
 from dotenv import load_dotenv
 
+# Initialize Flask app and load environment variables
 app = Flask(__name__)
 load_dotenv()
 
+# Set OpenAI API key
 openai.api_key = os.environ.get('OPENAI_KEY')
 
+# Function to format duration string into a human-readable format
 def format_duration(duration_string):
     duration = isodate.parse_duration(duration_string)
     total_seconds = int(duration.total_seconds())
@@ -25,6 +28,7 @@ def format_duration(duration_string):
     else:
         return f"{seconds}s"
 
+# Function to format view count into a human-readable format
 def format_view_count(view_count):
     view_count = int(view_count)
     if view_count >= 1000000:
@@ -34,10 +38,12 @@ def format_view_count(view_count):
     else:
         return str(view_count)
 
+# Function to format date string into a human-readable format
 def format_date(date_string):
     date = datetime.datetime.fromisoformat(date_string[:-1])
     return date.strftime("%B %d, %Y")
 
+# Function to parse transcript and extract text information
 def parse_text_info(input_list):
     pattern = re.compile(r"'text':\s+'(?:\[[^\]]*\]\s*)?([^']*)'")
     output = ""
@@ -50,7 +56,12 @@ def parse_text_info(input_list):
             output += text + " "
     return output.strip()
 
+# Function to generate summary using OpenAI API
 def generate_summary(captions, summary_length):
+    # Set default  length to 200 tokens
+    default_value = 200
+    # Set summary length to default value if user does not select a summary length
+    summary_length = summary_length if summary_length is not None else default_value
     prompt = f"These are captions for a youtube video, can you provide a summary on this youtube video based on the closed captions provided here:\n\n{captions}\n"
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -81,6 +92,7 @@ def get_transcript(path):
             id=video_id
         ).execute()
         
+        # Extract video information
         video_info = {
             'title': video_response['items'][0]['snippet']['title'],
             'author': video_response['items'][0]['snippet']['channelTitle'],
@@ -88,13 +100,19 @@ def get_transcript(path):
             'view_count': format_view_count(video_response['items'][0]['statistics']['viewCount']),
             'thumbnail': video_response['items'][0]['snippet']['thumbnails']['medium']['url'],
         }
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        captions = parse_text_info(transcript)
-        print(captions)
-        summary_length = int(request.form['summary_length'])
-        print("SUMMARY LENGTH HERE", summary_length)
-        summary = generate_summary(captions, summary_length)
-        return render_template('index.html', video_info=video_info, summary=summary)
+
+    # Get transcript and parse text
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    captions = parse_text_info(transcript)
+    print(captions)
+
+    # Generate summary based on user-selected summary length
+    summary_length = int(request.form['summary_length'])
+    print("SUMMARY LENGTH HERE", summary_length)
+    summary = generate_summary(captions, summary_length)
+
+    # Render the result in the template
+    return render_template('index.html', video_info=video_info, summary=summary)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
