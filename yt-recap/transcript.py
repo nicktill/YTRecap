ef571af1f4103c1316f05a2e64ae882cc03b5c24
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-03-23 14:41:20 -0400

    adding gitignore

--- yt-recap/transcript.py
+++ yt-recap/transcript.py
@@ -1,4 +1,3 @@
-# Import required libraries
 import os
 import openai
 from flask import Flask, render_template, request
@@ -9,26 +8,16 @@ import datetime
 import isodate
 from dotenv import load_dotenv
 
-# Initialize the Flask app
 app = Flask(__name__)
-
 load_dotenv()
 
-
-# Set OpenAI API key
 openai.api_key = os.environ.get('OPENAI_KEY')
-print("API KEY HERE" , openai.api_key)
 
-# Define a function to format video duration
 def format_duration(duration_string):
-    # Parse the duration string
     duration = isodate.parse_duration(duration_string)
-    # Convert the duration to total seconds
     total_seconds = int(duration.total_seconds())
-    # Extract hours, minutes, and seconds
     hours, remainder = divmod(total_seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
-    # Format the duration based on the number of hours, minutes, and seconds
     if hours > 0:
         return f"{hours}h {minutes}m {seconds}s"
     elif minutes > 0:
@@ -36,11 +25,8 @@ def format_duration(duration_string):
     else:
         return f"{seconds}s"
 
-# Define a function to format video view count
 def format_view_count(view_count):
-    # Convert the view count to an integer
     view_count = int(view_count)
-    # Format the view count based on its value
     if view_count >= 1000000:
         return f"{view_count // 1000000}M"
     elif view_count >= 10000:
@@ -48,19 +34,13 @@ def format_view_count(view_count):
     else:
         return str(view_count)
 
-# Define a function to format video publish date
 def format_date(date_string):
-    # Convert the date string to a datetime object
     date = datetime.datetime.fromisoformat(date_string[:-1])
-    # Format the date string
     return date.strftime("%B %d, %Y")
 
 def parse_text_info(input_list):
-    # Define a regular expression pattern to match the text information
     pattern = re.compile(r"'text':\s+'(?:\[[^\]]*\]\s*)?([^']*)'")
-    # Initialize the output variable
     output = ""
-    # Loop through the input list and match the text information
     for item in input_list:
         match = pattern.search(str(item))
         if match:
@@ -68,16 +48,10 @@ def parse_text_info(input_list):
             text = text.replace('\n', ' ')
             text = re.sub(' +', ' ', text)
             output += text + " "
-    # Return the parsed text information
     return output.strip()
 
-# Define a function to generate a summary of the video based on its closed captions
 def generate_summary(captions, summary_length):
-    # Define the prompt for the OpenAI API
     prompt = f"These are captions for a youtube video, can you provide a summary on this youtube video based on the closed captions provided here:\n\n{captions}\n"
-    print(prompt)
-
-    # Generate a summary using the OpenAI API
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -86,36 +60,27 @@ def generate_summary(captions, summary_length):
         stop=None,
         temperature=0.5,
     )
-    # Extract the summary text from the API response
     summary = response.choices[0].text.strip()
-    # Return the summary text
     return summary
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    # Render the index.html template
     return render_template('index.html')
 
 @app.route('/', methods=['POST'], defaults={'path': ''})
 @app.route('/<path:path>', methods=['POST'])
 def get_transcript(path):
     url = request.form['url']
-    # Use regular expressions to extract the video ID from the URL
     match = re.search(r"(?<=v=)[\w-]+|[\w-]+(?<=/v/)|(?<=youtu.be/)[\w-]+", url)
     if match:
-        # Extract the video ID from the regular expression match
         video_id = match.group(0)
-        # Retrieve video information from YouTube Data API
         youtube = build('youtube', 'v3', developerKey=os.environ.get('YT_KEY'))
-        youtube_api_key = os.environ.get('YT_KEY')
-        print("YOUTUBE KEY HERE", youtube_api_key)
         video_response = youtube.videos().list(
             part='snippet,statistics',
             id=video_id
         ).execute()
         
-        # Extract relevant information from video response
         video_info = {
             'title': video_response['items'][0]['snippet']['title'],
             'author': video_response['items'][0]['snippet']['channelTitle'],
@@ -123,18 +88,12 @@ def get_transcript(path):
             'view_count': format_view_count(video_response['items'][0]['statistics']['viewCount']),
             'thumbnail': video_response['items'][0]['snippet']['thumbnails']['medium']['url'],
         }
-        # Retrieve the video transcript from YouTube
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        # Parse the text information from the transcript
         captions = parse_text_info(transcript)
         print(captions)
-        # Get the summary length from the form data
         summary_length = int(request.form['summary_length'])
         print("SUMMARY LENGTH HERE", summary_length)
-        # Generate a summary based on the closed captions
         summary = generate_summary(captions, summary_length)
-
-        # Render the index.html template with the video information and summary
         return render_template('index.html', video_info=video_info, summary=summary)
 
 if __name__ == '__main__':
