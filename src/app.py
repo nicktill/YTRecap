ce5ed42b7306383d5e7a2aef3ad6c5f4d98576c0
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-04-12 19:24:20 -0400

    🔧fix mobile bug

--- src/app.py
+++ src/app.py
@@ -236,4 +236,4 @@ def getNewLengthSummary():
 
 # Run Flask app
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 4200)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
